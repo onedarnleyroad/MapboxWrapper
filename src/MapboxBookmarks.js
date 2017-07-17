0f4ec09c29d643a3e68b9c52a904d1defe41807d
Nathan Edwards <nathan@onedarnleyroad.com>
2017-07-17 17:29:31 +0100

    remove reliance on APP

--- src/MapboxBookmarks.js
+++ src/MapboxBookmarks.js
@@ -4,7 +4,21 @@ module.exports = (function() {
 
 
 	var MapboxBookmarks = function( options ) {
-		this.map = options.map;
+		this.map = h_getProp('map', options );
+        this.animSpeed = h_getProp('animSpeed', options );
+
+        if (!this.animSpeed) {
+            this.animSpeed = 4000;
+        };
+
+        this.mobileCheck = h_getProp('animSpeed', options );
+
+        if (typeof this.mobileCheck != "function") {
+            this.mobileCheck = function() {
+                return window.matchMedia( '(max-width: 600px)' ).matches
+            };
+        }
+
 		this.bookmarks = {};
 
 		var self = this;
@@ -44,16 +58,16 @@ module.exports = (function() {
 					var loc = b.location;
 					var z = b.zoom
 
-					if ( b.locationMobile && APP.media.matches('mobile') ) {
+					if ( b.locationMobile && self.mobileCheck() ) {
 						loc = b.locationMobile;
 					}
 
-					if ( b.zoomMobile && APP.media.matches('mobile') ) {
+					if ( b.zoomMobile && self.mobileCheck()) {
 						z = b.zoomMobile;
 					}
 
 					self.map.flyTo({
-						speed: APP.config.animSpeed,
+						speed: self.animSpeed,
 						zoom: z,
 						center: loc
 					});
