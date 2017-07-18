d32ee9c445c09da56c5c4fa2a467716b2b5088ec
Nathan Edwards <nathan@onedarnleyroad.com>
2017-07-18 10:51:54 +0100

    build mapbox.js from source for convenience

--- build.js
+++ build.js
@@ -13,7 +13,41 @@ var outputFs = fs.createWriteStream( dest );
 b.add( src );
 b.bundle().pipe( outputFs );
 
-outputFs.on('finish', function () {
-    console.log('Finished building: ' + dest);
+
+
+
+
+
+// Build mapbox.js
+
+var l = browserify({
+    standalone: 'L'
+});
+
+var loutputFs = fs.createWriteStream( __dirname + "/dist/mapbox.js" );
+
+l.add( './node_modules/mapbox.js/src/index.js' );
+l.bundle().pipe( loutputFs );
+
+
+
+
+var p1 = new Promise(function( res, rej ) {
+    outputFs.on('finish', function () {
+        console.log('Finished building: ' + dest);
+        res();
+    });
+});
+
+var p2 = new Promise(function( res, rej ) {
+    loutputFs.on('finish', function () {
+        console.log('Finished building: mapbox.js');
+        res();
+    });
+});
+
+
+Promise.all([p1,p2]).then(function() {
+    console.log("Built everything, exiting...");
     process.exit();
 });
