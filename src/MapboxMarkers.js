9a2f7d226114ba7c4fbf4280b6099ae17a3b23c4
Nathan Edwards <nathan@onedarnleyroad.com>
2017-07-19 10:24:08 +0100

    refactor markers, using proper location for extending the bounds for better cross-library compatability. Simplify the code, don't store a bunch of variables needlessly.

--- src/MapboxMarkers.js
+++ src/MapboxMarkers.js
@@ -29,6 +29,7 @@ module.exports = (function() {
 		this.addClass =  h._getProp( 'addClass', options ) ;
 
 		this.markers = [];
+        this.count = 0;
 
         this.bounds = this.map.LngLatBounds();
 
@@ -43,41 +44,42 @@ module.exports = (function() {
 
 		var self = this;
 
-		var thisMarker;
-
-		var addClass = self.addClass;
-		var template = self.template;
-		var elementCallback = self.elementCallback;
-		var onClick = self.onClick;
-
-        self.bounds.extend( markerData.location );
-
-		var thisMarker = self.map.addMarker( markerData.location, markerData, template );
+        // Create the marker, add to map:
+		var thisMarker = self.map.addMarker( markerData.location, markerData, self.template );
 		thisMarker._addTo( self.map );
 
+        // Extend the stored bounds
+        self.bounds.extend( thisMarker.getLngLat() );
+
+        // Get the dom element..
 		var $el = $( thisMarker.getElement() );
 
-		if ( addClass ) {
-			$el.addClass( addClass );
+        // Add a class?
+		if ( self.addClass ) {
+			$el.addClass( self.addClass );
 		}
 
-		if (typeof elementCallback === 'function') {
-			elementCallback( thisMarker, $el, markerData );
+        // Run a callback on the marker?
+		if (typeof self.elementCallback === 'function') {
+			self.elementCallback( thisMarker, $el, markerData );
 		}
 
-		if (typeof onClick === 'function') {
-			thisMarker.onClick( onClick );
+        // run a click handler on the marker?
+		if (typeof self.onClick === 'function') {
+			thisMarker.onClick( self.onClick );
 		}
 
+        // return the marker to whatever called this:
 		return thisMarker;
 
 	};
 
 	MapboxMarkers.prototype.addMarkers = function() {
-
+        // run through this.locations, plotting each one, and adding it to the big array.
 		var self = this;
 		for (var id in self.locations) {
 			self.markers.push( self.plot( self.locations[id] ) );
+            self.count = self.markers.length;
 		}
 	};
 
